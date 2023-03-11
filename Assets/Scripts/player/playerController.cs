488dd2d3a7c8ce6141774e0f2c24243e70eb2e05
William Fecteau <william-fecteau@hotmail.com>
2023-03-11 09:28:13 -0500

    Fix tilemap Collisions and player movement

--- Assets/Scripts/player/playerController.cs
+++ Assets/Scripts/player/playerController.cs
@@ -2,36 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody2D rigidbody;
 
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector3 position = transform.position;
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) inputVector.y = 1;
+        if (Input.GetKey(KeyCode.S)) inputVector.y = -1;
+        if (Input.GetKey(KeyCode.A)) inputVector.x = -1;
+        if (Input.GetKey(KeyCode.D)) inputVector.x = 1;
 
-        if (Input.GetKey(KeyCode.W)) {
-            position.y += speed * Time.deltaTime;
-        }
+        inputVector = inputVector.normalized;
 
-        if (Input.GetKey(KeyCode.A)) {
-            position.x -= speed * Time.deltaTime;
-        }
 
-        if (Input.GetKey(KeyCode.S)) {
-            position.y -= speed * Time.deltaTime;
+        Vector2 movementVector = Vector2.zero;
+        movementVector.x = inputVector.x;
+        movementVector.y = inputVector.y;
+        movementVector = movementVector * speed * Time.deltaTime;
 
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            position.x += speed * Time.deltaTime;
-        }
-
-        transform.position = position;
+        rigidbody.MovePosition(rigidbody.position + movementVector);
     }
 }
