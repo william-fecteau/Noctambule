a30178add63e4f65da5daa5fc61bd72cd8d2ca73
Nicolas Lauzon <nilau28@ulaval.ca>
2023-03-11 13:11:54 -0500

    WIP

--- Assets/Scripts/ShopTrigger.cs
+++ Assets/Scripts/ShopTrigger.cs
@@ -6,15 +6,12 @@ public class ShopTrigger : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Triggered");
-        // 3 is the shop layer. Make sure it is set correctly in the inspector
         if (other.CompareTag("Player")) {
             ShopManager.GetInstance().ToggleShop();
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        // 3 is the shop layer. Make sure it is set correctly in the inspector
         if (other.CompareTag("Player")) {
             ShopManager.GetInstance().ToggleShop();
         }
