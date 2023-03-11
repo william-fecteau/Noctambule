62466ae2021844397a1785f323d5d8fc9b34de56
William Fecteau <william-fecteau@hotmail.com>
2023-03-11 00:43:51 -0500

    Reformat du code

--- Assets/Scripts/AudioScript/Playlist.cs
+++ Assets/Scripts/AudioScript/Playlist.cs
@@ -20,21 +20,19 @@ using UnityEngine;
 public class Playlist : MonoBehaviour
 {
 
-    public AudioClip[] playlist;
+    [SerializeField] private AudioClip[] playlist;
 
-    public AudioClip randomClip;
+    [SerializeField] private AudioClip randomClip;
 
-    public RandomAudioClip playlistRandom;
+    //[SerializeField] private RandomAudioClip playlistRandom;
 
-    public AudioSource currentSong;
+    [SerializeField] private AudioSource currentSong;
 
     // Start is called before the first frame update
     void Start()
     {
-        RandomAudioClip playlistRandom = new RandomAudioClip();
         currentSong = new AudioSource();
-       
-        Debug.Log(playlist);
+
         randomClip = playlist[Random.Range(0, playlist.Length-1)];
 
         currentSong = gameObject.AddComponent<AudioSource>();
@@ -54,63 +52,63 @@ public class Playlist : MonoBehaviour
     }
 }
 
-[System.Serializable]
-public class RandomAudioClip
-{
+//[System.Serializable]
+//public class RandomAudioClip
+//{
 
-    public AudioClip[] audioClipArray;
+//    private AudioClip[] audioClipArray;
 
-    private int audioClipIndex;
-    private int[] previousArray;
-    private int previousArrayIndex;
+//    private int audioClipIndex;
+//    private int[] previousArray;
+//    private int previousArrayIndex;
 
-    // The best random method
-    public AudioClip GetRandomAudioClip()
-    {
-        // Initialize
-        if (previousArray == null)
-        {
-            // Sets the length to half of the number of AudioClips
-            // This will round downwards
-            // So it works with odd numbers like for example 3
-            previousArray = new int[audioClipArray.Length / 2];
-        }
-        if (previousArray.Length == 0)
-        {
-            // If the the array length is 0 it returns null
-            return null;
-        }
-        else
-        {
-            // Psuedo random remembering previous clips to avoid repetition
-            do
-            {
-                audioClipIndex = Random.Range(0, audioClipArray.Length);
-            } while (PreviousArrayContainsAudioClipIndex());
-            // Adds the selected array index to the array
-            previousArray[previousArrayIndex] = audioClipIndex;
-            // Wrap the index
-            previousArrayIndex++;
-            if (previousArrayIndex >= previousArray.Length)
-            {
-                previousArrayIndex = 0;
-            }
-        }
+//    // The best random method
+//    public AudioClip GetRandomAudioClip()
+//    {
+//        // Initialize
+//        if (previousArray == null)
+//        {
+//            // Sets the length to half of the number of AudioClips
+//            // This will round downwards
+//            // So it works with odd numbers like for example 3
+//            previousArray = new int[audioClipArray.Length / 2];
+//        }
+//        if (previousArray.Length == 0)
+//        {
+//            // If the the array length is 0 it returns null
+//            return null;
+//        }
+//        else
+//        {
+//            // Psuedo random remembering previous clips to avoid repetition
+//            do
+//            {
+//                audioClipIndex = Random.Range(0, audioClipArray.Length);
+//            } while (PreviousArrayContainsAudioClipIndex());
+//            // Adds the selected array index to the array
+//            previousArray[previousArrayIndex] = audioClipIndex;
+//            // Wrap the index
+//            previousArrayIndex++;
+//            if (previousArrayIndex >= previousArray.Length)
+//            {
+//                previousArrayIndex = 0;
+//            }
+//        }
 
-        // Returns the randomly selected clip
-        return audioClipArray[audioClipIndex];
-    }
+//        // Returns the randomly selected clip
+//        return audioClipArray[audioClipIndex];
+//    }
 
-    // Returns if the randomIndex is in the array
-    private bool PreviousArrayContainsAudioClipIndex()
-    {
-        for (int i = 0; i < previousArray.Length; i++)
-        {
-            if (previousArray[i] == audioClipIndex)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+//    // Returns if the randomIndex is in the array
+//    private bool PreviousArrayContainsAudioClipIndex()
+//    {
+//        for (int i = 0; i < previousArray.Length; i++)
+//        {
+//            if (previousArray[i] == audioClipIndex)
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
